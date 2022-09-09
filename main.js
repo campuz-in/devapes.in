93ba335bff8869645d790fd420bbb257b48effe2
rubanthilak <hello@rubi.land>
2022-09-09 15:33:00 +0530

    beta release

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ $(document).on('click', 'a[href^="#"]', function (event) {
   event.preventDefault();
   handleSidebar()
   $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - 84
+      scrollTop: $($.attr(this, 'href')).offset().top
   }, 600);
 });
 
@@ -29,3 +29,43 @@ $('.option').on('click', function(){
     this.classList.add("active")
   }
 })
+
+$('#submit').on('click', contactUsToday)
+
+async function contactUsToday() {
+
+  let name = $('#name').val()
+  let phone = $('#phone').val()
+  let email = $('#email').val()
+  let details = $('#project-content').val()
+
+  if(!email.trim()) return
+
+  let postURL = `https://firestore.googleapis.com/v1/projects/devapes-in/databases/(default)/documents/clients?key=${import.meta.env.VITE_API_KEY}&documentId=${email}`;
+  let options = {
+    method:'POST',
+    body: JSON.stringify({
+      fields: {
+        name: { stringValue: name },
+        phone: { stringValue: phone },
+        email: { stringValue: email },
+        details: { stringValue: details },
+      }
+    }),
+    contentType: "application/json",
+    muteHttpExceptions:true
+  }
+
+  let res = await fetch(postURL,options)
+  if(res.ok) {
+    alert("We will get back to you ASAP !")
+    $('#name').val("")
+    $('#phone').val("")
+    $('#email').val("")
+    $('#project-content').val("")
+  }
+  else {
+    alert("Something went wrong. Please reach out to us : contact@devapes.in")
+  }
+}
+
